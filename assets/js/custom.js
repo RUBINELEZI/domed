af8fab6a07520bda6ba6cf5f750d0c087a015fd8
rubin <rubinelezi0@gmai.com>
2020-07-19 20:57:43 +0200

    call it a day

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -232,3 +232,18 @@
 }); 	
 })(jQuery);
 
+
+//popup
+const wp = document.querySelectorAll('#wp')
+const gen = document.querySelector('#general')
+
+gen.addEventListener('click' , () =>{
+    wp.forEach(w =>{
+        if(w.classList.contains('show')){
+            w.classList.remove('show')
+        }else{
+            w.classList.add('show')
+        }
+    })
+    console.log(wp);
+})
